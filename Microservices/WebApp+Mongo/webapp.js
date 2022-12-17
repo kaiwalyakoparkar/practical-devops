f22eef6d472d31501b43bf4d6ed7c36ced4f63b1
Kaiwalya Koparkar <kaiwalyakoparkar@gmail.com>
2022-12-17 14:46:35 +0530

    feat: Added reference application for mongo connection

---
+++ Microservices/WebApp+Mongo/webapp.js
@@ -0,0 +1,84 @@
+let express = require('express');
+let path = require('path');
+let fs = require('fs');
+let MongoClient = require('mongodb').MongoClient;
+let app = express();
+
+// bodyParser was deprecated: https://stackoverflow.com/a/59892173
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+  });
+
+app.get('/profile-picture', function (req, res) {
+  let img = fs.readFileSync(path.join(__dirname, "images/profile-1.jpg"));
+  res.writeHead(200, {'Content-Type': 'image/jpg' });
+  res.end(img, 'binary');
+});
+
+// use when starting application locally with node command
+let mongoUrlLocal = "mongodb://admin:password@localhost:27017";
+
+// use when starting application as a separate docker container
+let mongoUrlDocker = "mongodb://admin:password@host.docker.internal:27017";
+
+// use when starting application as docker container, part of docker-compose
+let mongoUrlDockerCompose = "mongodb://admin:password@mongodb";
+
+let mongoUrlK8s = `mongodb://${process.env.USER_NAME}:${process.env.USER_PWD}@${process.env.DB_URL}`
+
+// pass these options to mongo client connect request to avoid DeprecationWarning for current Server Discovery and Monitoring engine
+let mongoClientOptions = { useNewUrlParser: true, useUnifiedTopology: true };
+
+// "user-account" in demo with docker. "my-db" in demo with docker-compose
+let databaseName = "my-db";
+
+app.post('/update-profile', function (req, res) {
+  let userObj = req.body;
+
+  MongoClient.connect(mongoUrlK8s, mongoClientOptions, function (err, client) {
+    if (err) throw err;
+
+    let db = client.db(databaseName);
+    userObj['userid'] = 1;
+
+    let myquery = { userid: 1 };
+    let newvalues = { $set: userObj };
+
+    db.collection("users").updateOne(myquery, newvalues, {upsert: true}, function(err, res) {
+      if (err) throw err;
+      client.close();
+    });
+
+  });
+  // Send response
+  res.send(userObj);
+});
+
+app.get('/get-profile', function (req, res) {
+  let response = {};
+  // Connect to the db
+  MongoClient.connect(mongoUrlK8s, mongoClientOptions, function (err, client) {
+    if (err) throw err;
+
+    let db = client.db(databaseName);
+
+    let myquery = { userid: 1 };
+
+    db.collection("users").findOne(myquery, function (err, result) {
+      if (err) throw err;
+      response = result;
+      client.close();
+
+      // Send response
+      res.send(response ? response : {});
+    });
+  });
+});
+
+app.listen(3000, function () {
+  console.log("app listening on port 3000!");
+});
+
