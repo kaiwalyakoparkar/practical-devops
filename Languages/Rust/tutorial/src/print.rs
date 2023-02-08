3f042ec6a19c89eb70ef1b45b2f5d26fc526a01c
Kaiwalya Koparkar <kaiwalyakoparkar@gmail.com>
2023-02-08 17:42:51 +0530

    feat: Multiple placeholders

--- Languages/Rust/tutorial/src/print.rs
+++ Languages/Rust/tutorial/src/print.rs
@@ -8,4 +8,7 @@ pub fn run() {
     println!("{}", 1);
     println!("Hello my name is {}", "Kaiwalya");
     println!("{} is learning {}", "Kaiwalya", "Rust");
+
+    //Multiple placeholders
+    println!("{0} is learning {1}, {0} is {2}", "Kaiwalya", "Rust", "student")
 }
