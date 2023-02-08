0bdd3c5b7da9f11205727200916765c3999dd018
Kaiwalya Koparkar <kaiwalyakoparkar@gmail.com>
2023-02-08 18:05:52 +0530

    feat: Variables and constants

--- Languages/Rust/tutorial/src/vars.rs
+++ Languages/Rust/tutorial/src/vars.rs
@@ -4,5 +4,15 @@
 
 pub fn run() {
     let name = "Kaiwalya";
-    println!("My name is {}", name);
+    let age = 19;
+    let mut pencil = 3;
+
+    println!("My name is {} and I am {} years old and has {} pencils", name, age, pencil);
+
+    //Generates error coz it's trying the change the variable value
+    //age = 30;
+
+    //Will not generate error coz of mut (mutable option added to it)
+    pencil = 5;
+    println!("But now he has {} pencils", pencil);
 }
