2b5aa5a02903ac10ae520c6b8b7254ba0fa72f94
Kaiwalya Koparkar <kaiwalyakoparkar@gmail.com>
2023-02-09 18:34:53 +0530

    feat: Functions

--- Languages/Rust/tutorial/src/main.rs
+++ Languages/Rust/tutorial/src/main.rs
@@ -3,11 +3,12 @@
 // mod vars;
 // mod types;
 // mod string;
-mod loops;
+// mod loops;
 // mod tuples;
 // mod arrays;
 // mod vectors;
 // mod conditionals;
+mod functions;
 
 fn main() {
 
@@ -19,10 +20,11 @@ fn main() {
     // vars::run();
     // types::run();
     // string::run();
-    loops::run();
+    // loops::run();
     // tuples::run();
     // arrays::run();
     // vectors::run();
     // conditionals::run();
+    functions::run();
 
 }
