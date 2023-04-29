af0aeb93c91bb353ef0f68ae21512580d33938d3
Kaiwalya Koparkar <kaiwalyakoparkar@gmail.com>
2023-04-29 14:10:24 +0530

    feat: datatype dones

--- Languages/Go/Basics/datatype.go
+++ Languages/Go/Basics/datatype.go
@@ -7,8 +7,26 @@ func main ()  {
 	var b bool = false
 	var f float64 = 77.90
 
+	//Assign value afterwards
 	var name string
 	name = "Koparkar"
+
+	//Shorthand
+	//1
+	var v,p int = 3, 4
+	fmt.Println(v,p);
+
+	//2
+	var (
+		st string = "foo"
+		val int = 54
+	)
+	fmt.Println(st, val);
+
+
+	//3
+	s := "Kaiwalya Koparkar"
+	fmt.Println(s);
 
 	fmt.Print(b)
 	fmt.Print("Welcome ", str, " your marks are ", i, "\n");
