95714487dce0705b279231842731e460b38adb43
Kaiwalya Koparkar <kaiwalyakoparkar@gmail.com>
2023-04-29 19:50:44 +0530

    feat: multi user input

--- Languages/Go/Basics/user-input.go
+++ Languages/Go/Basics/user-input.go
@@ -3,7 +3,13 @@ import "fmt"
 
 func main() {
 	var name string
-	fmt.Print("Enter your namve: ")
+	var is_muggle bool
+	fmt.Print("Enter your name: ")
 	fmt.Scanf("%s", &name)//Takes 1 input from user
 	fmt.Println("Hey there, ", name)
+
+	//Multiple inputs
+	fmt.Print("Enter your name and muggle status: ")
+	fmt.Scanf("%s %t", &name, &is_muggle)
+	fmt.Println("Hello ",name," your muggle status is ", is_muggle)
 }
