f769b68c1c1ede42e0fa4be92a55d7f263f28d26
Kaiwalya Koparkar <kaiwalyakoparkar@gmail.com>
2023-04-30 18:16:42 +0530

    feat: switch case added

---
+++ Languages/Go/Basics/switchCase.go
@@ -0,0 +1,15 @@
+package main
+import "fmt"
+
+func main() {
+	i:=100
+
+	switch i {
+		case 10:
+			fmt.Println("i is 10")
+		case 20:
+			fmt.Println("i is 10")
+		default:
+			fmt.Println("i is neither 10 nor 20")
+	}
+}
