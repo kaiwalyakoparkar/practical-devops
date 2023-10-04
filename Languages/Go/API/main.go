9be7f89b659d924af394c541012e244a6f95ce2e
Kaiwalya Koparkar <kaiwalyakoparkar@gmail.com>
2023-10-05 00:17:40 +0530

    feat: Simpler Restful api added

--- Languages/Go/API/main.go
+++ Languages/Go/API/main.go
@@ -1,57 +1,64 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"log"
 	"net/http"
-	"github.com/gorilla/mux"
+
+	"github.com/gin-gonic/gin"
 )
 
-type Product struct {
-	Id string
-	Name string
-	Quantity int
-	Price float64
-} 
 
-var Products []Product;
 
-func homepage(w http.ResponseWriter, r *http.Request) {
-	log.Println("Endpoint hit: homepage")
-	fmt.Fprintf(w, "Welcome to the homepage")
+type album struct {
+	ID string `json: "id"`
+	Title string `json: "title"`
+	Artist string `json: "artist"`
+	Price float64 `json: "price"`
 }
 
-func returnAllProducts(w http.ResponseWriter, r *http.Request) {
-	log.Println("Endpoint hit: returnAllProducts")
-	json.NewEncoder(w).Encode(Products)
+var albums = []album {
+	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
 }
 
-func returnProductById(w http.ResponseWriter, r *http.Request) {
-	log.Println(r.URL.Path)
-	vars := mux.Vars(r)
-	key := vars["id"]
-	for _, product := range Products {
-		if product.Id == key {
-			json.NewEncoder(w).Encode(product)
+func postAlbums(c *gin.Context) {
+	var newAlbum album
+
+	if err := c.BindJSON(&newAlbum); err != nil {
+		return
+	}
+
+	albums = append(albums, newAlbum)
+	c.IndentedJSON(http.StatusCreated, newAlbum)
+}
+
+func main()  {
+	router := gin.Default()
+	router.GET("/albums", getAlbums)
+	router.POST("/albums", postAlbums)
+	router.GET("/health", getHealth)
+	router.GET("/albums/:id", getAlbumByID)
+	router.Run("localhost:8080")
+}
+
+func getAlbums(c *gin.Context)  {
+	c.IndentedJSON(http.StatusOK, albums)
+}
+
+
+func getHealth(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, gin.H{"message": "Your stuff works"})
+}
+
+func getAlbumByID(c *gin.Context) {
+	id := c.Param("id")
+
+	for _, a := range albums {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
 		}
 	}
-}
 
-func handleRequests() {
-	myRouter := mux.NewRouter().StrictSlash(true)
-	myRouter.HandleFunc("/products", returnAllProducts)
-	myRouter.HandleFunc("/product/{id}", returnProductById)
-	myRouter.HandleFunc("/", homepage)
-	http.ListenAndServe("localhost:10000", myRouter)
-}
-
-func main() {
-
-	Products = []Product{
-		{Id: "1", Name: "Chair", Quantity: 100, Price: 100.00},
-		{Id: "2", Name: "Desk", Quantity: 200, Price: 200.00},
-	}
-
-	handleRequests();
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Id you are trying to find is not available"})
 }
